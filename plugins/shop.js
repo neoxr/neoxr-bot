251c17560e1030cc12ae90a539bec3d9154885d9
neoxr <neoxr@null.net>
2022-07-12 08:57:05 +0700

    +

---
+++ plugins/shop.js
@@ -0,0 +1,12 @@
+exports.run = {
+   usage: ['premium'],
+   async: async (m, {
+      client,
+      isPrefix
+   }) => {
+      client.reply(m.chat, `Upgrade to premium plan at a price of Rp. 5,000 for 1.000 Limit, send *${isPrefix}owner* if want to buy.`, m)
+   },
+   error: false,
+   cache: true,
+   location: __filename
+}
