83b56e3a34046357e414dec6b0ede21d6d842f44
neoxr <neoxr@null.net>
2022-07-17 13:58:39 +0700

    fix: broadcast

--- plugins/owner/broadcast.js
+++ plugins/owner/broadcast.js
@@ -17,14 +17,14 @@ exports.run = {
                await Func.delay(1500)
                await client.sendMessageModify(jid, text, null, {
                   title: '© neoxr-bot v2.2.0 (Public Bot)',
-                  thumbnail: 'https://telegra.ph/file/aa76cce9a61dc6f91f55a.jpg',
+                  thumbnail: await Func.fetchBuffer('https://telegra.ph/file/aa76cce9a61dc6f91f55a.jpg'),
                   largeThumb: true,
                   url: 'https://chat.whatsapp.com/Dh1USlrqIfmJT6Ji0Pm2pP'
-               }, command == 'bcgc' ? ({
+               }, command == 'bcgc' ? {
                   contextInfo: {
                      mentionedJid: await (await client.groupMetadata(jid)).participants.map(v => v.id)
                   }
-               }) : {})
+               } : {})
             }
             client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : 'groups'}`), m)
          } else if (!/image\/(webp)/.test(mime)) {
@@ -32,11 +32,11 @@ exports.run = {
                await Func.delay(1500)
                let media = await q.download()
                await client.sendFile(jid, media, '', q.text ? '乂  *B R O A D C A S T*\n\n' + q.text : '', null, null,
-                  command == 'bcgc' ? ({
+                  command == 'bcgc' ? {
                      contextInfo: {
                         mentionedJid: await (await client.groupMetadata(jid)).participants.map(v => v.id)
                      }
-                  }) : {})
+                  } : {})
             }
             client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : 'groups'}`), m)
          } else client.reply(m.chat, Func.texted('bold', `🚩 Media / text not found or media is not supported.`), m)
