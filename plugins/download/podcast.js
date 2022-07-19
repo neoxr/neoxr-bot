5f60d7cc974ab7b806d70debbfbc8c6e6b4f3231
neoxr <neoxr@null.net>
2022-07-19 10:50:07 +0700

    some fixed

--- plugins/download/podcast.js
+++ plugins/download/podcast.js
@@ -7,7 +7,7 @@ exports.run = {
       command
    }) => {
       try {
-         if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9iOTRlYWMwL3BvZGNhc3QvcnNz/episode/NGY4M2VmYTktMTMzZi00ZTZmLWJjNmItYmZiZmU3NjQyNTMy'), m)
+         if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9jYjQ0OGM0L3BvZGNhc3QvcnNz/episode/MjhjODNkYzUtMGVhNi00MWEwLTk0YWMtYzhmZjBkOWNkYTM1?sa=X&ved=0CAUQkfYCahcKEwj4pdqlhYT5AhUAAAAAHQAAAAAQLA'), m)
          if (!args[0].match(/^(?:https?:\/\/)?(?:podcasts\.)?(?:google\.com\/)(?:feed\/)(?:\S+)?$/)) return client.reply(m.chat, global.status.invalid, m)
          client.sendReact(m.chat, '🕒', m.key)
          let json = await Api.podcast(args[0])
