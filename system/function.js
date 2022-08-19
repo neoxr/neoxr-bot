ff4d227103ceae98dd1047f54b49dfd5477dee6a
neoxr <neoxr@null.net>
2022-08-19 12:15:52 +0700

    random id

--- system/function.js
+++ system/function.js
@@ -507,4 +507,18 @@ module.exports = class Function {
    switcher = (status, isTrue, isFalse) => {
       return (status) ? this.texted('bold', isTrue) : this.texted('bold', isFalse)
    }
+   
+   /* Random ID
+    * @param {Integer} length
+    */ 
+   makeId = (length) => {
+      var result = ''
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+      var charactersLength = characters.length
+      for (var i = 0; i < length; i++) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength))
+      }
+      return result
+   }
+
 }
