e5b875ea44c5d9d33fa4d8f6e1c57ec55b2e9ddc
neoxr <neoxr@null.net>
2022-08-26 07:32:13 +0700

    replit config

--- replit.nix
+++ replit.nix
@@ -2,8 +2,16 @@
 	deps = [
 		pkgs.nodejs-14_x
         pkgs.ffmpeg
+        pkgs.libwebp
+        pkgs.imagemagick
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.replitPackages.jest
+        pkgs.libuuid
 	];
+	env = {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.libuuid
+        ];
+    };
 }
