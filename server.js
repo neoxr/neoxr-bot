d42db3711e9d53d39d217cabb9014be309fc4b26
neoxr <neoxr@null.net>
2022-08-27 23:29:41 +0700

    keep alive

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 3000
+
+server.set('json spaces', 2)
+server.all('/', (req, res) => {
+   res.setHeader('Content-Type', 'text/html')
+   res.json({
+      status: 'alive'
+   })
+   res.end()
+})
+
+function keepAlive() {
+   server.listen(PORT, () => {
+      console.log(`Server running with port ${PORT}!`)
+   })
+}
+
+module.exports = keepAlive
