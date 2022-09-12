e95743e5dd7815fe0550848dc7f4f911dfd1ab7a
neoxr <neoxr@null.net>
2022-09-12 13:07:02 +0700

    add: brainly

--- plugins/tools/brainly.js
+++ plugins/tools/brainly.js
@@ -9,6 +9,7 @@ exports.run = {
       try {
          if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'jokowi'), m)
          const json = await Func.fetchJson('https://neoxr.masuk.id/brainly?q=' + text)
+         client.sendReact(m.chat, '🕒', m.key)
          if (!json.status) return client.reply(m.chat, global.status.fail, m)
          for (let i = 0; i < 3; i++) {
             await Func.delay(1500)
