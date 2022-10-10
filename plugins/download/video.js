f6135cf432612070477440774101e2baf9e12163
neoxr <neoxr@null.net>
2022-10-10 08:27:36 +0700

    update yt api

--- plugins/download/video.js
+++ plugins/download/video.js
@@ -1,6 +1,4 @@
-const { decode } = require('html-entities')
-const { ytv } = require('../../lib/y2mate')
-const yt = require('usetube')
+cconst yt = require('usetube')
 exports.run = {
    usage: ['video'],
    hidden: ['playvid', 'playvideo'],
@@ -17,31 +15,24 @@ exports.run = {
          client.sendReact(m.chat, '🕒', m.key)
          const search = await (await yt.searchVideo(text)).videos
          if (!search || search.length == 0) return client.reply(m.chat, global.status.fail, m)
-         const {
-            dl_link,
-            thumb,
-            title,
-            duration,
-            filesizeF
-         } = await ytv('https://youtu.be/' + search[0].id)
-         if (!dl_link) return client.reply(m.chat, global.status.fail, m)
+         const json = await Func.fetchJson('https://yt.nxr.my.id/yt2?url=https://youtu.be/' + search[0].id + '&type=video')
+         if (!json.status || !json.data.url) return client.reply(m.chat, global.status.fail, m)
          let caption = `乂  *Y T - V I D E O*\n\n`
-         caption += `	◦  *Title* : ${decode(title)}\n`
-         caption += `	◦  *Size* : ${filesizeF}\n`
-         caption += `	◦  *Duration* : ${duration}\n`
-         caption += `	◦  *Quality* : 480p\n\n`
+         caption += `	◦  *Title* : ${json.title}\n`
+         caption += `	◦  *Size* : ${json.data.size}\n`
+         caption += `	◦  *Duration* : ${json.duration}\n`
+         caption += `	◦  *Quality* : ${json.data.quality}\n\n`
          caption += global.footer
-         let chSize = Func.sizeLimit(filesizeF, global.max_upload)
-         if (chSize.oversize) return client.reply(m.chat, `💀 File size (${filesizeF}) exceeds the maximum limit, download it by yourself via this link : ${await (await scrap.shorten(dl_link)).data.url}`, m)
-         let isSize = (filesizeF).replace(/MB/g, '').trim()
+         let chSize = Func.sizeLimit(json.data.size, global.max_upload)
+         if (chSize.oversize) return client.reply(m.chat, `💀 File size (${json.data.size}) exceeds the maximum limit, download it by yourself via this link : ${await (await scrap.shorten(json.data.url)).data.url}`, m)
+         let isSize = (json.data.size).replace(/MB/g, '').trim()
          if (isSize > 99) return client.sendMessageModify(m.chat, caption, m, {
-            title: '© neoxr-bot v2.2.0 (Public Bot)',
             largeThumb: true,
-            thumbnail: await Func.fetchBuffer(thumb)
-         }).then(async () => await client.sendFile(m.chat, dl_link, decode(title) + '.mp4', '', m, {
+            thumbnail: await Func.fetchBuffer(json.thumbnail)
+         }).then(async () => await client.sendFile(m.chat, json.data.url, json.data.filename, '', m, {
             document: true
          }))
-         client.sendFile(m.chat, dl_link, Func.filename('mp4'), caption, m)
+         client.sendFile(m.chat, json.data.url, json.data.filename, caption, m)
       } catch (e) {
          console.log(e)
          return client.reply(m.chat, global.status.error, m)
