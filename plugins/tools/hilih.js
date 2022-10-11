5bbd0aace21d39fbf6ebb0ae8c83ca8ec3f8e996
neoxr <neoxr@null.net>
2022-10-11 14:08:55 +0700

    fix group stealer

--- plugins/tools/hilih.js
+++ plugins/tools/hilih.js
@@ -9,14 +9,13 @@ exports.run = {
       command
    }) => {
       try {
-         if (!text || (!m.quoted && !m.quoted.text)) return client.reply(m.chat, Func.example(isPrefix, command, 'How are you?'), m)
          let vocal = command.charAt(1)
          let t = m.quoted ? m.quoted.text : text
+         if (!t) return client.reply(m.chat, Func.example(isPrefix, command, 'How are you?'), m)
          client.reply(m.chat, t.toLowerCase().replace(/[aiueo]/gi, vocal), m)
-      } catch {
-         client.reply(m.chat, global.status.error, m)
+      } catch (e) {
+         client.reply(m.chat, Func.jsonFormat(e), m)
       }
    },
-   error: false,
-   group: true
+   error: false
 }
