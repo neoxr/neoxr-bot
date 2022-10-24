be962e4f64d92a9500cf684d7869e5cb6b76abf0
neoxr <neoxr@null.net>
2022-10-24 21:32:42 +0700

    automatic data check and deletion every 1 minute

--- plugins/event/system/clearDB.js
+++ plugins/event/system/clearDB.js
@@ -7,13 +7,13 @@ exports.run = {
             let day = 86400000 * 7,
                now = new Date() * 1
             for (let jid in global.db.users) {
-               if (now - global.db.users[jid].lastseen > week && !global.db.users[jid].premium && !global.db.users[jid].banned && global.db.users[jid].point < 5000000) delete global.db.users[jid]
+               if (now - global.db.users[jid].lastseen > day && !global.db.users[jid].premium && !global.db.users[jid].banned && global.db.users[jid].point < 5000000) delete global.db.users[jid]
             }
             for (let jid in global.db.chats) {
                if (now - global.db.chats[jid].lastseen > day) delete global.db.chats[jid]
             }
             for (let jid in global.db.groups) {
-               if (now - global.db.groups[jid].activity > week && !global.db.groups[jid].stay && global.db.groups[jid].expired == 0) {
+               if (now - global.db.groups[jid].activity > day && !global.db.groups[jid].stay && global.db.groups[jid].expired == 0) {
                   client.groupLeave(jid)
                   delete global.db.groups[jid]
                }
