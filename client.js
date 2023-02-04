47c3b2e65bd1aa5fe95a57cfb818aa76a58925cc
neoxr <neoxr@null.net>
2023-02-04 18:51:04 +0700

    + fix error

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ require('./system/config')
 if (process.env.DATABASE_URL) {
    MongoDB.db = global.database
 }
-global.props = (process.env.DATABASE_URL && /mongo/.test(process.env.DATABASE_URL)) ? MongoDB : (process.env.DATABASE_URL && /postgres/.test(process.env.DATABASE_URL)) ? PostgreSQL : new(require('./lib/system/localdb'))(global.database)
+global.props = (process.env.DATABASE_URL && /mongo/.test(process.env.DATABASE_URL)) ? MongoDB : (process.env.DATABASE_URL && /postgres/.test(process.env.DATABASE_URL)) ? PostgreSQL : new(require('./system/localdb'))(global.database)
 global.scrap = Scraper
 global.store = makeInMemoryStore({
    logger: pino().child({
