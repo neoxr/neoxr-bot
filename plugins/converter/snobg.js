ad43154338361ad48a6a122ec622a4e4f7a63d24
neoxr <neoxr@null.net>
2023-02-14 02:51:02 +0700

    fix snobg

--- plugins/converter/snobg.js
+++ plugins/converter/snobg.js
@@ -4,19 +4,16 @@ exports.run = {
    category: 'converter',
    async: async (m, {
       client,
-      text,
       isPrefix,
       command
    }) => {
       try {
          let exif = global.db.setting
-         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'Hi | Dude'), m)
-         client.sendReact(m.chat, '🕒', m.key)
-         let [top, bottom] = text.split`|`
          if (m.quoted ? m.quoted.message : m.msg.viewOnce) {
             let type = m.quoted ? Object.keys(m.quoted.message)[0] : m.mtype
             let q = m.quoted ? m.quoted.message[type] : m.msg
             if (/image/.test(type)) {
+           	client.sendReact(m.chat, '🕒', m.key)
                let img = await client.downloadMediaMessage(q)
                let url = await scrap.uploadImage(img)
                let json = await Api.nobg(url)
@@ -31,6 +28,7 @@ exports.run = {
             let mime = (q.msg || q).mimetype || ''
             if (!mime) return client.reply(m.chat, Func.texted('bold', `🚩 Reply photo.`), m)
             if (!/image\/(jpe?g|png)/.test(mime)) return client.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
+            client.sendReact(m.chat, '🕒', m.key)
             let img = await q.download()
             let url = await scrap.uploadImage(img)
             let json = await Api.nobg(url)
