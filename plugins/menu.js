34c274277a011387990e80913f0391d8aaf9cca7
neoxr <neoxr@null.net>
2023-02-17 17:34:45 +0700

    ^ update menu

--- plugins/menu.js
+++ plugins/menu.js
@@ -12,7 +12,7 @@ exports.run = {
          const id = m.chat
          const local_size = fs.existsSync('./' + global.database + '.json') ? await Func.getSize(fs.statSync('./' + global.database + '.json').size) : ''
          const library = JSON.parse(require('fs').readFileSync('./package.json', 'utf-8'))
-         const message = global.db.setting.msg.replace('+tag', `@${m.sender.replace(/@.+/g, '')}`).replace('+name', m.pushName).replace('+greeting', Func.greeting()).replace('+db', (process.env.DATABASE_URL ? 'Mongo' : `Local (${local_size})`)).replace('+version', (library.dependencies.bails ? library.dependencies.bails : library.dependencies.baileys).replace('^', '').replace('~', ''))
+         const message = global.db.setting.msg.replace('+tag', `@${m.sender.replace(/@.+/g, '')}`).replace('+name', m.pushName).replace('+greeting', Func.greeting()).replace('+db', (process.env.DATABASE_URL ? 'Mongo' : `Local (${local_size})`)).replace('+version', (library.dependencies.bails ? library.dependencies.bails : library.dependencies['@adiwajshing/baileys'] ? '@adiwajshing/baileys' : library.dependencies.baileys).replace('^', '').replace('~', ''))
          const style = global.db.setting.menuStyle
          if (style == 1) {
             if (text) {
