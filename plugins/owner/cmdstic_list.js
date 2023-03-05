00d7cb273e75eaa4bc9086fdf1378e8a3e5c7c50
neoxr <neoxr@null.net>
2023-03-05 09:51:42 +0700

    +

--- plugins/owner/cmdstic_list.js
+++ plugins/owner/cmdstic_list.js
@@ -14,7 +14,7 @@ exports.run = {
          teks += '	◦  ' + Func.texted('bold', 'Text') + ' : ' + global.db.sticker[cmdS[i]].text + '\n'
          teks += '	◦  ' + Func.texted('bold', 'Created') + ' : ' + moment(global.db.sticker[cmdS[i]].created).format('DD/MM/YY HH:mm:ss') + '\n\n'
       }
-      client.fakeStory(m.chat, teks + global.db.setting.footer, global.db.setting.header)
+      m.reply(teks + global.db.setting.footer)
    },
    owner: true
 }
