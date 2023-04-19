72c27736317d292d1033d193340c653454dd70de
neoxr <neoxr@null.net>
2023-04-19 18:36:25 +0700

    + admin action

--- plugins/admin/action.js
+++ plugins/admin/action.js
@@ -1,5 +1,5 @@
 exports.run = {
-   usage: ['demote', 'kick'],
+   usage: ['add', 'promote', 'demote', 'kick'],
    use: 'mention or reply',
    category: 'admin tools',
    async: async (m, {
@@ -9,19 +9,29 @@ exports.run = {
       command,
       participants
    }) => {
-      let number = m.quoted ? m.quoted.sender : m.mentionedJid.length != 0 ? m.mentionedJid[0] : isNaN(text) ? text.replace(/[()+\s-]/g, '') + '@s.whatsapp.net' : text
-      if (!text && !m.quoted) return client.reply(m.chat, Func.texted('bold', `🚩 Mention or reply target`), m)
-      if (await client.onWhatsApp(number).length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Number not registered on WhatsApp.`), m)
-      try {
-         let mention = number.replace(/@.+/g, '')
-         let users = m.isGroup ? participants.find(u => u.id == number) : {}
-         if (!users) return client.reply(m.chat, Func.texted('bold', `🚩 @${mention} already logged out or does not exist in this group.`), m)
-         if (number  == client.decodeJid(client.user.id))return client.reply(m.chat, Func.texted('bold', `??`), m)
-         if (command == 'kick') return client.groupParticipantsUpdate(m.chat, [number], 'remove')
-         if (command == 'demote') return client.groupParticipantsUpdate(m.chat, [number], 'demote')
-      } catch (e) {
-      	console.log(e)
-      	client.reply(m.chat, global.status.error, m)
+      let input = text ? text : m.quoted ? m.quoted.sender : m.mentionedJid.length > 0 ? m.mentioneJid[0] : false
+      if (!input) return client.reply(m.chat, Func.texted('bold', `🚩 Mention or reply chat target.`), m)
+      let p = await client.onWhatsApp(input.trim())
+      if (p.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Invalid number.`), m)
+      let jid = client.decodeJid(p[0].jid)
+      let number = jid.replace(/@.+/, '')
+      if (command == 'kick') {
+         let member = participants.find(u => u.id == jid)
+         if (!member) return client.reply(m.chat, Func.texted('bold', `🚩 @${number} already left or does not exist in this group.`), m)
+         client.groupParticipantsUpdate(m.chat, [jid], 'remove').then(res => m.reply(Func.jsonFormat(res)))
+      } else if (command == 'add') {
+         // if (!isOwner) return client.reply(m.chat, global.status.owner, m)
+         let member = participants.find(u => u.id == jid)
+         if (member) return client.reply(m.chat, Func.texted('bold', `🚩 @${number} already in this group.`), m)
+         client.groupParticipantsUpdate(m.chat, [jid], 'add').then(res => m.reply(Func.jsonFormat(res)))
+      } else if (command == 'demote') {
+         let member = participants.find(u => u.id == jid)
+         if (!member) return client.reply(m.chat, Func.texted('bold', `🚩 @${number} already left or does not exist in this group.`), m)
+         client.groupParticipantsUpdate(m.chat, [jid], 'demote').then(res => m.reply(Func.jsonFormat(res)))
+      } else if (command == 'promote') {
+         let member = participants.find(u => u.id == jid)
+         if (!member) return client.reply(m.chat, Func.texted('bold', `🚩 @${number} already left or does not exist in this group.`), m)
+         client.groupParticipantsUpdate(m.chat, [jid], 'promote').then(res => m.reply(Func.jsonFormat(res)))
       }
    },
    group: true,
