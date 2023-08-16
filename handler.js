a658ea7500b01977befe8b9240c512941d087736
neoxr <neoxr@null.net>
2023-08-17 06:34:27 +0700

    change: baileys package and @neoxr/wb version

--- handler.js
+++ handler.js
@@ -191,7 +191,7 @@ module.exports = async (client, ctx) => {
          }
       }
    } catch (e) {
-      if (/(overlimit|timeout|users|item|time)/ig.test(e.message)) return
+      if (/(undefined|overlimit|timed|timeout|users|item|time)/ig.test(e.message)) return
       console.log(e)
       if (!m.fromMe) return m.reply(Func.jsonFormat(new Error('neoxr-bot encountered an error :' + e)))
    }
