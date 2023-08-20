76af52708f656269677ba1c2e196618d023a3779
neoxr <neoxr@null.net>
2023-08-20 13:36:56 +0700

    add: bard & ocr

---
+++ plugins/utilities/ocr.js
@@ -0,0 +1,48 @@
+exports.run = {
+   usage: ['ocr'],
+   use: 'reply photo',
+   category: 'utilities',
+   async: async (m, {
+      client,
+      isPrefix,
+      command,
+      Func,
+      Scraper
+   }) => {
+      try {
+         if (m.quoted ? m.quoted.message : m.msg.viewOnce) {
+            let type = m.quoted ? Object.keys(m.quoted.message)[0] : m.mtype
+            let q = m.quoted ? m.quoted.message[type] : m.msg
+            if (/image/.test(type)) {
+           	client.sendReact(m.chat, '🕒', m.key)
+               let img = await client.downloadMediaMessage(q)
+               let image = await Scraper.uploadImageV2(img)
+               const json = await Api.neoxr('/ocr', {
+                  image: image.data.url
+               })
+               if (!json.status) return m.reply(Func.jsonFormat(json))
+               client.reply(m.chat, json.data.text, m)
+            } else client.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
+         } else {
+            let q = m.quoted ? m.quoted : m
+            let mime = (q.msg || q).mimetype || ''
+            if (!mime) return client.reply(m.chat, Func.texted('bold', `🚩 Reply photo.`), m)
+            if (!/image\/(jpe?g|png)/.test(mime)) return client.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
+            client.sendReact(m.chat, '🕒', m.key)
+            let img = await q.download()
+            let image = await Scraper.uploadImageV2(img)
+            const json = await Api.neoxr('/ocr', {
+               image: image.data.url
+            })
+            if (!json.status) return m.reply(Func.jsonFormat(json))
+            client.reply(m.chat, json.data.text, m)
+         }
+      } catch (e) {
+         return client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   error: false,
+   limit: true,
+   cache: true,
+   location: __filename
+}
