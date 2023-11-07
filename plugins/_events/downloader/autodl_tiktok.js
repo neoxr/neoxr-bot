66371b5c2c333f261c98c45e428d6c9e7c467343
neoxr-v2 <tykotz@null.net>
2023-11-07 08:26:00 +0700

    feat: update tiktok & @neoxr/wb library

--- plugins/_events/downloader/autodl_tiktok.js
+++ plugins/_events/downloader/autodl_tiktok.js
@@ -10,7 +10,7 @@ exports.run = {
    }) => {
       try {
          const regex = /^(?:https?:\/\/)?(?:www\.|vt\.|vm\.|t\.)?(?:tiktok\.com\/)(?:\S+)?$/;
-         const extract = body ? Func.generateLink(body) : null
+         const extract = (m.quoted && m.quoted.text) ? Func.generateLink(m.quoted.text) : body ? Func.generateLink(body) : null
          if (extract) {
             const links = extract.filter(v => Func.ttFixed(v).match(regex))
             if (links.length != 0) {
@@ -25,26 +25,30 @@ exports.run = {
                Func.hitstat('tiktok', m.sender)
                links.map(async link => {
                   const json = await Api.neoxr('/tiktok', {
-                  	url: Func.ttFixed(link)
+                     url: Func.ttFixed(link)
                   })
                   if (!json.status) return client.reply(m.chat, Func.texted('bold', `🚩 Error! private videos or videos not available.`), m)
-                  let caption = `乂  *T I K T O K*\n\n`
-                  caption += `	◦  *Author* : ${json.data.author.nickname} (@${json.data.author.username})\n`
-                  caption += `	◦  *Views* : ${Func.formatter(json.data.stats.play_count)}\n`
-                  caption += `	◦  *Likes* : ${Func.formatter(json.data.stats.digg_count)}\n`
-                  caption += `	◦  *Shares* : ${Func.formatter(json.data.stats.share_count)}\n`
-                  caption += `	◦  *Comments* : ${Func.formatter(json.data.stats.comment_count)}\n`
-                  caption += `	◦  *Duration* : ${Func.toTime(json.data.duration)}\n`
-                  caption += `	◦  *Sound* : ${json.data.music.title} - ${json.data.music.author}\n`
-                  caption += `	◦  *Caption* : ${json.data.caption || '-'}\n`
-                  caption += `	◦  *Fetching* : ${((new Date - old) * 1)} ms\n\n`
-                  caption += global.footer
-                  if (json.data.video) return client.sendFile(m.chat, json.data.video, 'video.mp4', caption, m)
-                  if (json.data.photo) {
-                     for (let p of json.data.photo) {
-                        client.sendFile(m.chat, p, 'image.jpg', caption, m)
-                        await Func.delay(1500)
+                  if (json.data.stats) {
+                     let caption = `乂  *T I K T O K*\n\n`
+                     caption += `	◦  *Author* : ${json.data.author.nickname} (@${json.data.author.username})\n`
+                     caption += `	◦  *Views* : ${Func.formatter(json.data.stats.play_count)}\n`
+                     caption += `	◦  *Likes* : ${Func.formatter(json.data.stats.digg_count)}\n`
+                     caption += `	◦  *Shares* : ${Func.formatter(json.data.stats.share_count)}\n`
+                     caption += `	◦  *Comments* : ${Func.formatter(json.data.stats.comment_count)}\n`
+                     caption += `	◦  *Duration* : ${Func.toTime(json.data.duration)}\n`
+                     caption += `	◦  *Sound* : ${json.data.music.title} - ${json.data.music.author}\n`
+                     caption += `	◦  *Caption* : ${json.data.caption || '-'}\n`
+                     caption += `	◦  *Fetching* : ${((new Date - old) * 1)} ms\n\n`
+                     caption += global.footer
+                     if (json.data.video) return client.sendFile(m.chat, json.data.video, 'video.mp4', caption, m)
+                     if (json.data.photo) {
+                        for (let p of json.data.photo) {
+                           client.sendFile(m.chat, p, 'image.jpg', caption, m)
+                           await Func.delay(1500)
+                        }
                      }
+                  } else {
+                     client.sendFile(m.chat, json.data.url, '', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
                   }
                })
             }
--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -18,29 +18,33 @@ exports.run = {
          const json = await Api.neoxr('/tiktok', {
             url: Func.ttFixed(args[0])
          })
-         if (!json.status || !json.data.video) return client.reply(m.chat, Func.jsonFormat(json), m)
-         let caption = `乂  *T I K T O K*\n\n`
-         caption += `	◦  *Author* : ${json.data.author.nickname} (@${json.data.author.username})\n`
-         caption += `	◦  *Views* : ${Func.formatter(json.data.stats.play_count)}\n`
-         caption += `	◦  *Likes* : ${Func.formatter(json.data.stats.digg_count)}\n`
-         caption += `	◦  *Shares* : ${Func.formatter(json.data.stats.share_count)}\n`
-         caption += `	◦  *Comments* : ${Func.formatter(json.data.stats.comment_count)}\n`
-         caption += `	◦  *Duration* : ${Func.toTime(json.data.duration)}\n`
-         caption += `	◦  *Sound* : ${json.data.music.title} - ${json.data.music.author}\n`
-         caption += `	◦  *Caption* : ${json.data.caption || '-'}\n`
-         caption += `	◦  *Fetching* : ${((new Date - old) * 1)} ms\n\n`
-         caption += global.footer
-         if (command == 'tiktok' || command == 'tt') {
-            if (json.data.video) return client.sendFile(m.chat, json.data.video, 'video.mp4', caption, m)
-            if (json.data.photo) {
-               for (let p of json.data.photo) {
-                  client.sendFile(m.chat, p, 'image.jpg', caption, m)
-                  await Func.delay(1500)
+         if (!json.status) return client.reply(m.chat, Func.texted('bold', `🚩 Error! private videos or videos not available.`), m)
+         if (json.data.stats) {
+            let caption = `乂  *T I K T O K*\n\n`
+            caption += `	◦  *Author* : ${json.data.author.nickname} (@${json.data.author.username})\n`
+            caption += `	◦  *Views* : ${Func.formatter(json.data.stats.play_count)}\n`
+            caption += `	◦  *Likes* : ${Func.formatter(json.data.stats.digg_count)}\n`
+            caption += `	◦  *Shares* : ${Func.formatter(json.data.stats.share_count)}\n`
+            caption += `	◦  *Comments* : ${Func.formatter(json.data.stats.comment_count)}\n`
+            caption += `	◦  *Duration* : ${Func.toTime(json.data.duration)}\n`
+            caption += `	◦  *Sound* : ${json.data.music.title} - ${json.data.music.author}\n`
+            caption += `	◦  *Caption* : ${json.data.caption || '-'}\n`
+            caption += `	◦  *Fetching* : ${((new Date - old) * 1)} ms\n\n`
+            caption += global.footer
+            if (command == 'tiktok' || command == 'tt') {
+               if (json.data.video) return client.sendFile(m.chat, json.data.video, 'video.mp4', caption, m)
+               if (json.data.photo) {
+                  for (let p of json.data.photo) {
+                     client.sendFile(m.chat, p, 'image.jpg', caption, m)
+                     await Func.delay(1500)
+                  }
                }
             }
+            if (command == 'tikwm') return client.sendFile(m.chat, json.data.videoWM, 'video.mp4', caption, m)
+            if (command == 'tikmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
+         } else {
+            client.sendFile(m.chat, json.data.url, '', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
          }
-         if (command == 'tikwm') return client.sendFile(m.chat, json.data.videoWM, 'video.mp4', caption, m)
-         if (command == 'tikmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
       } catch (e) {
          return client.reply(m.chat, Func.jsonFormat(e), m)
       }
