5d17443a26bc54b218a1d22515097a79ea0f48b5
galalio445 <theworldh95@gmail.com>
2024-08-09 15:16:09 +0500

    asd

--- plugins/download/test.js
+++ plugins/download/test.js
@@ -1,51 +1,21 @@
-const axios = require('axios'); // Import axios library
-const { youtube } = require('@xct007/frieren-scraper'); // Import the youtube function from the scraper package
+const { youtube } = require('@xct007/frieren-scraper');
 
 exports.run = {
-    usage: ['play'],
-    hidden: ['lagu', 'song'],
-    use: 'query',
+    usage: ['test'],
+    use: 'app id',
     category: 'downloader',
-    async: async (m, { client, text, isPrefix, command, users, env, Func, Scraper }) => {
+    async: async (m, { client, args, text, isPrefix, command, Func }) => {
         try {
-            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'lathi'), m);
-            client.sendReact(m.chat, '🕒', m.key);
+            const Obj = await youtube.download("https://www.youtube.com/watch?v=E8rpY2FwKkY");
+console.log(Obj);
 
-            // Step 1: Perform YouTube search using the @xct007/frieren-scraper package
-            const searchResults = await youtube.search(text);
-
-            // Get the first search result
-            const firstResult = searchResults[0];
-
-            // Step 2: Use the first search result's URL to fetch the download link
-            const downloadResponse = await axios.get(`https://api.betabotz.eu.org/api/download/ytmp3?url=${firstResult.url}&apikey=beta-Ibrahim1209`);
-            const downloadUrl = downloadResponse.data.result.mp3;
-
-            let caption = `乂  *Y T - P L A Y*\n\n`;
-            caption += `    ◦  *Title* : ${firstResult.title}\n`;
-            caption += `    ◦  *Uploader* : N/A\n`;  // Uploader information isn't available, so this is set to N/A
-            caption += `    ◦  *Duration* : ${firstResult.duration}\n`;
-            caption += `    ◦  *Views* : ${firstResult.views}\n`;
-            caption += `    ◦  *Uploaded* : ${firstResult.uploaded}\n`;
-            caption += global.footer;
-
-            
-                client.sendFile(m.chat, downloadUrl, `${firstResult.title}.mp3`, '', m, {
-                    document: false,
-                    APIC: await Func.fetchBuffer(firstResult.thumbnail)
-                });
-            
         } catch (e) {
-            console.log(e);
-            return client.reply(m.chat, Func.jsonFormat(e), m);
+            console.error(e);
+            return client.reply(m.chat, global.status.error, m);
         }
     },
     error: false,
     limit: true,
-    restrict: true,
-    restrict: true,
     verified: true,
-    cache: true,
-    cache: true,
-    location: __filename
+    premium: false
 };
