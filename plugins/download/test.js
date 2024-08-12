987e16938b9430ae7e32a4e25b58c9d05eba2972
galalio445 <theworldh95@gmail.com>
2024-08-12 22:03:41 +0500

    asd

---
+++ plugins/download/test.js
@@ -0,0 +1,23 @@
+
+const apk_dl = require('apk-dl.js');
+exports.run = {
+    usage: ['test'],
+    hidden: ['playvid', 'playvideo'],
+    use: 'query',
+    category: 'feature',
+    async: async (m, { client, text, isPrefix, command, env, users, Scraper, Func }) => {
+        try {
+            
+            apk_dl.apkdl.search('minecraft').then((response) => {
+                console.log(response.message);
+              });
+        } catch (e) {
+            console.error(e);
+            return client.reply(m.chat, Func.jsonFormat(e), m);
+        }
+    },
+    error: false,
+    restrict: true,
+    cache: true,
+    location: __filename
+};
