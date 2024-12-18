bc943417053c55dca2237f34215f40cd73b8a939
neoxr <admin@neoxr.my.id>
2024-12-18 03:34:45 +0000

    chore: update connection + support button on @neoxr/wb & implement sendFromAI function

---
+++ plugins/fromAI.js
@@ -0,0 +1,18 @@
+exports.run = {
+   usage: ['fromai'],
+   async: async (m, {
+      client,
+      Func
+   }) => {
+      try {
+         // only work in private chat
+         client.sendFromAI(m.chat, 'Hi!', m)
+      } catch (e) {
+         client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+   },
+   error: false,
+   private: true,
+   cache: true,
+   location: __filename
+}
