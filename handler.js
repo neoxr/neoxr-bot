7c3948866f8e73b56a131e0619609a9daf911fe3
neoxr <neoxr@null.net>
2025-05-28 10:56:12 +0000

    chore(deps): bump @neoxr/wb 5.7.17 to 5.8.22
    bump @neoxr/baileys 7.9.18 to 8.1.2
    
    avoid undefined id & lid parser

--- handler.js
+++ handler.js
@@ -21,7 +21,7 @@ module.exports = async (client, ctx) => {
       let isOwner = [client.decodeJid(client.user.id).replace(/@.+/, ''), env.owner, ...setting.owners].map(v => v + '@s.whatsapp.net').includes(m.sender)
       let isPrem = users && users.premium || isOwner
       let groupMetadata = m.isGroup ? await client.getGroupMetadata(m.chat) : {}
-      let participants = m.isGroup ? groupMetadata ? groupMetadata.participants : [] : [] || []
+      let participants = m.isGroup ? groupMetadata ? client.lidParser(groupMetadata.participants) : [] : [] || []
       let adminList = m.isGroup ? await client.groupAdmin(m.chat) : [] || []
       let isAdmin = m.isGroup ? adminList.includes(m.sender) : false
       let isBotAdmin = m.isGroup ? adminList.includes((client.user.id.split`:`[0]) + '@s.whatsapp.net') : false
