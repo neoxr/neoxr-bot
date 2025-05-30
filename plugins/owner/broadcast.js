aee9f1baf16ebea4d25d5388d4f2e181f7fbbaf0
neoxr <neoxr@null.net>
2025-05-30 13:26:43 +0000

    refactor: broadcast

--- plugins/owner/broadcast.js
+++ plugins/owner/broadcast.js
@@ -9,63 +9,84 @@ exports.run = {
       Func
    }) => {
       try {
-         let q = m.quoted ? m.quoted : m
-         let mime = (q.msg || q).mimetype || ''
-         let chatJid = global.db.chats.filter(v => v.jid && v.jid.endsWith('.net')).map(v => v.jid)
-         let groupList = async () => Object.entries(await client.groupFetchAllParticipating()).slice(0).map(entry => entry[1])
-         let groupJid = await (await groupList()).map(v => v.id)
+         const chatJid = global.db.chats.filter(v => v.jid && v.jid.endsWith('.net')).map(v => v.jid)
+         const groupJid = Object.entries(await client.groupFetchAllParticipating()).map(entry => entry[1])
          const id = command == 'bc' ? chatJid : groupJid
-         if (id.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Error, ID does not exist.`), m)
+         if (!id?.length) return client.reply(m.chat, Func.texted('bold', `🚩 Error, ID does not exist.`), m)
+         const group = (command == 'bcgc' || command == 'bcgcv')
+         const q = m.quoted ? m.quoted : m
+         const mime = (q.msg || q).mimetype || ''
          client.sendReact(m.chat, '🕒', m.key)
-         if (text) {
+
+         if (/image\/(webp)/.test(mime)) {
+            client.sendReact(m.chat, '🕒', m.key)
             for (let jid of id) {
                await Func.delay(1500)
-               await client.sendMessageModify(jid, text, null, {
+               const member = group ? client.lidParser(jid?.participants)?.map(v => v.id) : []
+               let media = await q.download()
+               await client.sendSticker(group ? jid.id : jid, media, null, {
+                  packname: setting.sk_pack,
+                  author: setting.sk_author,
+                  mentions: command == 'bcgc' ? member : []
+               })
+            }
+            return client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : command === 'bcprem' ? 'premium users' : 'groups'}`), m)
+         }
+
+         if (/video|image\/(jpe?g|png)/.test(mime)) {
+            client.sendReact(m.chat, '🕒', m.key)
+            for (let jid of id) {
+               await Func.delay(1500)
+               const member = group ? client.lidParser(jid?.participants)?.map(v => v.id) : []
+               let media = await q.download()
+               await client.sendFile(group ? jid.id : jid, media, '', q?.text ? '乂  *B R O A D C A S T*\n\n' + q.text : (text || ''), null, { netral: true },
+                  command == 'bcgc'
+                     ? { contextInfo: { mentionedJid: member } }
+                     : command == 'bcgcv'
+                        ? { viewOnce: true, contextInfo: { mentionedJid: member } }
+                        : command == 'bcv'
+                           ? { viewOnce: true }
+                           : {}
+               )
+            }
+            return client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : command === 'bcprem' ? 'premium users' : 'groups'}`), m)
+         }
+
+         if (/audio/.test(mime)) {
+            client.sendReact(m.chat, '🕒', m.key)
+            for (let jid of id) {
+               await Func.delay(1500)
+               const member = group ? client.lidParser(jid?.participants)?.map(v => v.id) : []
+               let media = await q.download()
+               await client.sendFile(group ? jid.id : jid, media, '', '', null, { netral: true },
+                  command == 'bcgc'
+                     ? { ptt: q.ptt, contextInfo: { mentionedJid: member } }
+                     : command == 'bcgcv'
+                        ? { viewOnce: true, contextInfo: { mentionedJid: member } }
+                        : {}
+               )
+            }
+            return client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : command === 'bcprem' ? 'premium users' : 'groups'}`), m)
+         }
+
+         if (text) {
+            client.sendReact(m.chat, '🕒', m.key)
+            for (let jid of id) {
+               await Func.delay(1500)
+               const member = group ? client.lidParser(jid?.participants)?.map(v => v.id) : []
+               await client.sendMessageModify(group ? jid.id : jid, text, null, {
+                  netral: true,
                   title: global.botname,
                   thumbnail: await Func.fetchBuffer('https://telegra.ph/file/aa76cce9a61dc6f91f55a.jpg'),
                   largeThumb: true,
-                  url: global.db.setting.link,
-                  mentions: command == 'bcgc' ? await (await client.groupMetadata(jid)).participants.map(v => v.id) : []
+                  url: setting.link,
+                  mentions: command == 'bcgc' ? member : []
                })
             }
-            client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : 'groups'}`), m)
-         } else if (/image\/(webp)/.test(mime)) {
-            for (let jid of id) {
-               await Func.delay(1500)
-               let media = await q.download()
-               await client.sendSticker(jid, media, null, {
-                  packname: global.db.setting.sk_pack,
-                  author: global.db.setting.sk_author,
-                  mentions: command == 'bcgc' ? await (await client.groupMetadata(jid)).participants.map(v => v.id) : []
-               })
-            }
-            client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : 'groups'}`), m)
-         } else if (/video|image\/(jpe?g|png)/.test(mime)) {
-            for (let jid of id) {
-               await Func.delay(1500)
-               let media = await q.download()
-               await client.sendFile(jid, media, '', q.text ? '乂  *B R O A D C A S T*\n\n' + q.text : '', null, null,
-                  command == 'bcgc' ? {
-                     contextInfo: {
-                        mentionedJid: await (await client.groupMetadata(jid)).participants.map(v => v.id)
-                     }
-                  } : {})
-            }
-            client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : 'groups'}`), m)
-         } else if (/audio/.test(mime)) {
-            for (let jid of id) {
-               await Func.delay(1500)
-               let media = await q.download()
-               await client.sendFile(jid, media, '', '', null, null,
-                  command == 'bcgc' ? {
-                     ptt: q.ptt,
-                     contextInfo: {
-                        mentionedJid: await (await client.groupMetadata(jid)).participants.map(v => v.id)
-                     }
-                  } : {})
-            }
-            client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : 'groups'}`), m)
-         } else client.reply(m.chat, Func.texted('bold', `🚩 Media / text not found or media is not supported.`), m)
+            return client.reply(m.chat, Func.texted('bold', `🚩 Successfully send broadcast message to ${id.length} ${command == 'bc' ? 'chats' : command === 'bcprem' ? 'premium users' : 'groups'}`), m)
+         }
+
+         client.reply(m.chat, Func.texted('bold', `🚩 Use this command with text or by replying to an image, video or audio.`), m)
       } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
