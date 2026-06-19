d0a57c459ab7dd83f95ebe30c79a2ee7719c378b
neoxr <neoxr@null.net>
2026-06-19 07:10:37 +0000

    feat(add): read view-once

---
+++ plugins/_events/read_viewonce.js
@@ -0,0 +1,40 @@
+/*
+ * IMPORTANT: 
+ * This feature requires the orignal baileys library to function.
+ * Please refer to this PR for further information and updates:
+ * https://github.com/WhiskeySockets/Baileys/pull/2201
+ */
+
+export const run = {
+   async: async (m, {
+      client
+   }) => {
+      try {
+         if (!m.fromMe && m?.msg?.viewOnce) {
+            const type = Object.keys(m.message)?.[0]
+            const message = m.message?.[type]
+
+            if (!type || !message) return
+            delete message.viewOnce
+
+            const content = {
+               [type]: {
+                  ...message,
+                  contextInfo: {
+                     ...message.contextInfo,
+                     stanzaId: m?.key?.id || m?.id,
+                     participant: m?.key?.participant || m?.key?.remoteJid,
+                     quotedType: 0,
+                     quotedMessage: m?.message
+                  }
+               }
+            }
+
+            client.relayMessage(m.chat, content, {})
+         }
+      } catch (e) {
+         console.error(e)
+      }
+   },
+   error: false
+}
