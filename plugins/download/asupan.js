8a06f09b600c0fae4e9e9b6bfa72d3b48ce58ed9
neoxr <neoxr@null.net>
2026-06-20 03:18:26 +0000

    fix: asupan

--- plugins/download/asupan.js
+++ plugins/download/asupan.js
@@ -12,21 +12,24 @@ export const run = {
          // TikTok Username
          const json = await Api.neoxr('/asupan', {
             username: args[0] || Utils.random([
-               'itsbellefirst',
-               'hosico_cat'
+              'hosico_cat',
+              'dibdiby',
+              'bulansutena',
+              'sesaaak',
+              'ordinary307girl'
             ])
          })
          if (!json.status) return client.reply(m.chat, Utils.jsonFormat(json), m)
          let caption = `乂  *A S U P A N*\n\n`
-         caption += `	◦  *Author* : ${json.data.author.nickname} (@${json.data.author.username})\n`
-         caption += `	◦  *Views* : ${Utils.h2k(json.data.stats.play_count)}\n`
-         caption += `	◦  *Likes* : ${Utils.h2k(json.data.stats.digg_count)}\n`
-         caption += `	◦  *Shares* : ${Utils.h2k(json.data.stats.share_count)}\n`
-         caption += `	◦  *Comments* : ${Utils.h2k(json.data.stats.comment_count)}\n`
-         caption += `	◦  *Sound* : ${json.data.music.title} - ${json.data.music.authorName}\n`
+         caption += `	◦  *Author* : ${json.data.author.nickname} (@${json.data.author.uniqueId})\n`
+         caption += `	◦  *Views* : ${Utils.h2k(json.data.statistic.views)}\n`
+         caption += `	◦  *Likes* : ${Utils.h2k(json.data.statistic.likes)}\n`
+         caption += `	◦  *Shares* : ${Utils.h2k(json.data.statistic.shares)}\n`
+         caption += `	◦  *Comments* : ${Utils.h2k(json.data.statistic.comments)}\n`
+         caption += `	◦  *Sound* : ${json.data.music.title} - ${json.data.music.author}\n`
          caption += `	◦  *Caption* : ${json.data.caption || '-'}\n\n`
          caption += global.footer
-         client.sendFile(m.chat, json.data.video.url, '', caption, m)
+         client.sendFile(m.chat, json.data.video.url, 'video.mp4', caption, m)
       } catch (e) {
          return client.reply(m.chat, Utils.jsonFormat(e), m)
       }
